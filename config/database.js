79f3ff84da616eb599633f127c11eb1947c1f8d7
marlonzotty <marlinhoicloud243@gmail.com>
2023-10-31 17:06:43 -0300

    first commit

---
+++ config/database.js
@@ -0,0 +1,8 @@
+
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://127.0.0.1:27017/todo-list', { useNewUrlParser: true, useUnifiedTopology: true }) // Change 'userNewUrlParser' to 'useNewUrlParser'
+  .then(() => console.log('Conectado ao MongoDB'))
+  .catch((err) => console.error(err));
